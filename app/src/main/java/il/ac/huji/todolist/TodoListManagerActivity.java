c053fd67efe1a0b69900979d9633b96522599988
no4n6w <michael.braginsky@mail.huji.ac.il>
2015-03-17 17:32:35 +0200

    v1

---
+++ app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -0,0 +1,135 @@
+package il.ac.huji.todolist;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+
+public class TodoListManagerActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list_manager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_todo_list_manager, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        EditText edtNewItem;
+        edtNewItem = (EditText) findViewById(R.id.edtNewItem);
+        ListView lv;
+        lv = (ListView) findViewById(R.id.lstTodoItems);
+
+        String newTask = edtNewItem.getText().toString();
+
+        ArrayList<String> newList = new ArrayList<String>();
+        int numOfItems = lv.getCount();
+
+        for(int i=0; i<numOfItems; i++) {
+            String selectedFromList = (lv.getItemAtPosition(i).toString());
+            newList.add(selectedFromList);
+        }
+        newList.add(0,newTask);
+        edtNewItem.setText("");
+
+
+        ArrayAdapter<String> todoArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, newList){
+
+            @Override
+            public View getView(int position, View convertView,
+                                ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+                if ((position % 2) == 0) {
+                    textView.setTextColor(Color.RED);
+                } else {
+                    textView.setTextColor(Color.BLUE);
+                }
+                return view;
+            }
+        };
+
+        ListView list = (ListView)findViewById(R.id.lstTodoItems);
+        list.setAdapter(todoArrayAdapter);
+        registerForContextMenu(list);
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.getId() == R.id.lstTodoItems) {
+            ListView lv = (ListView) v;
+            AdapterView.AdapterContextMenuInfo contextMenuAdapter = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            menu.setHeaderTitle(lv.getItemAtPosition(contextMenuAdapter.position).toString());
+            menu.add("Delete Item");
+        }
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        ListView lv;
+        lv = (ListView) findViewById(R.id.lstTodoItems);
+
+        ArrayList<String> newList = new ArrayList<String>();
+        int numOfItems = lv.getCount();
+
+        for(int i=0; i<numOfItems; i++) {
+            String selectedFromList = (lv.getItemAtPosition(i).toString());
+            newList.add(selectedFromList);
+        }
+
+
+        ArrayAdapter<String> todoArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, newList){
+
+            @Override
+            public View getView(int position, View convertView,
+                                ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+                if ((position % 2) == 0) {
+
+
+            /*YOUR CHOICE OF COLOR*/
+                    textView.setTextColor(Color.RED);
+                } else {
+                    textView.setTextColor(Color.BLUE);
+                }
+                return view;
+            }
+        };
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        newList.remove(info.position);
+        ListView list = (ListView)findViewById(R.id.lstTodoItems);
+        list.setAdapter(todoArrayAdapter);
+        registerForContextMenu(list);
+        return super.onOptionsItemSelected(item);
+
+    }
+}
